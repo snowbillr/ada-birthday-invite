8c24f87cc6ef0447c418c255495fdc413868df58
Bill Reed <bill.reed@babylist.com>
2024-08-03 13:15:25 -0400

    docs

--- docs/index.66e6958d.js
+++
@@ -1,2 +0,0 @@
-!function(){let e=document.querySelector("#invitation"),t=document.querySelector("#authorization"),o=document.querySelector("input");document.querySelector("form").addEventListener("submit",n=>{n.preventDefault(),"bingo"===o.value.toLowerCase()&&(e.classList.remove("hidden"),t.classList.add("hidden"))})}();
-//# sourceMappingURL=index.66e6958d.js.map
