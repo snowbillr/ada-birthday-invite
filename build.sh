f8f0a1b97e85c032ea93aa21a7258ab4ef6f69f2
Bill Reed <bill.reed@babylist.com>
2024-08-03 13:28:25 -0400

    try again

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-npx parcel build index.html --dist-dir docs/ --public-url /ada-birthday-invite/
+# npx parcel build index.html --dist-dir docs/ --public-url /ada-birthday-invite/
+npx parcel index.html --dist-dir docs/ --public-url /ada-birthday-invite/
---
+++ docs/index.2fd0bd96.js
@@ -0,0 +1,17 @@
+(function() {
+    const invitationEl = document.querySelector("#invitation");
+    const authEl = document.querySelector("#authorization");
+    const passwordEl = document.querySelector("input");
+    document.querySelector("form").addEventListener("submit", (e)=>{
+        e.preventDefault();
+        if (passwordEl.value.toLowerCase() === "bingo") {
+            invitationEl.classList.remove("hidden");
+            authEl.classList.add("hidden");
+        } else {
+            alert("Try again!");
+            passwordEl.value = "";
+        }
+    });
+})();
+
+//# sourceMappingURL=index.2fd0bd96.js.map
